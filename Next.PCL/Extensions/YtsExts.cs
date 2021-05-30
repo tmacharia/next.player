4d8bf6ad1491c7171abc68ff5b2e3a1802344fba
tmacharia <timothy.macharia@outlook.com>
2021-05-30 08:51:16 +0300

    yify extension methods 4 images

---
+++ Next.PCL/Extensions/YtsExts.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using Next.PCL.Enums;
+using Next.PCL.Metas;
+using Next.PCL.Online.Models.Yts;
+
+namespace Next.PCL.Extensions
+{
+    internal static class YtsExts
+    {
+        internal static List<MetaImage> GetMetaImages(this YtsMovie model)
+        {
+            List<MetaImage> images = new List<MetaImage>();
+            if(model != null)
+            {
+                model.GetImageAs(x => x.Poster, MetaImageType.Poster, Resolution.WVGA, ref images);
+                model.GetImageAs(x => x.Poster_SM, MetaImageType.Poster, Resolution.WVGA, ref images);
+                model.GetImageAs(x => x.Poster_LG, MetaImageType.Poster, Resolution.HD, ref images);
+
+                model.GetImageAs(x => x.BackgroundImage, MetaImageType.Backdrop, Resolution.WVGA, ref images);
+                model.GetImageAs(x => x.BackgroundImageOriginal, MetaImageType.Backdrop, Resolution.HD, ref images);
+
+                model.GetImageAs(x => x.LargeScreenshotImage1, MetaImageType.Screenshot, Resolution.HD, ref images);
+                model.GetImageAs(x => x.LargeScreenshotImage2, MetaImageType.Screenshot, Resolution.HD, ref images);
+                model.GetImageAs(x => x.LargeScreenshotImage3, MetaImageType.Screenshot, Resolution.HD, ref images);
+            }
+            return images;
+        }
+        private static void GetImageAs(this YtsMovie model, Expression<Func<YtsMovie,Uri>> keySelector, MetaImageType type, Resolution resolution, ref List<MetaImage> images)
+        {
+            var url = model.GetPropValue(keySelector);
+            if(url != null)
+            {
+                images.Add(new MetaImage(type, MetaSource.YTS_MX)
+                {
+                    Url = url,
+                    Resolution = resolution
+                });
+            }
+        }
+    }
+}
