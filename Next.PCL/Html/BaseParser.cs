1fb75e7b490ee0dbfef59e3eaff07788dc385f56
tmacharia <timothy.macharia@outlook.com>
2021-06-02 09:29:54 +0300

    option to use HtmlWeb for html download

--- Next.PCL/Html/BaseParser.cs
+++ Next.PCL/Html/BaseParser.cs
@@ -1,4 +1,7 @@
-﻿using Common;
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Common;
 using HtmlAgilityPack;
 using Next.PCL.Online;
 
@@ -6,11 +9,34 @@ namespace Next.PCL.Html
 {
     public class BaseParser : BaseOnline
     {
+        /// <summary>
+        /// Calls the <paramref name="uri"/> and loads the results as a <see cref="HtmlDocument"/>
+        /// </summary>
+        /// <param name="uri"></param>
+        /// <param name="token">Cancellation token.</param>
+        /// <param name="useHtmlWeb4Download">Whether to use <see cref="HtmlWeb"/> for loading html from an online resource or the simple &amp; often quick get.</param>
+        /// <returns>A <see cref="HtmlDocument"/></returns>
+        protected virtual async Task<HtmlDocument> GetHtmlDocumentAsync(Uri uri, CancellationToken token = default, bool useHtmlWeb4Download = false)
+        {
+            if (!useHtmlWeb4Download)
+            {
+                string html = await GetAsync(uri, token);
+                return ConvertToHtmlDoc(html);
+            }
+            else
+            {
+                var wb = new HtmlWeb
+                {
+                    UsingCache = false
+                };
+                return await wb.LoadFromWebAsync(uri.OriginalString, token);
+            }
+        }
         protected virtual HtmlDocument ConvertToHtmlDoc(string html)
         {
             if (html.IsValid())
             {
-                HtmlDocument doc = new HtmlDocument();
+                var doc = new HtmlDocument();
                 doc.LoadHtml(html);
                 return doc;
             }
