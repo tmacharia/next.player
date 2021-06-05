fd0721c3d3ccb6014d558a8f79e6130fbfeb5b8d
tmacharia <timothy.macharia@outlook.com>
2021-06-05 06:25:16 +0300

    expressions evaluation of nested properties

--- Next.PCL/Extensions/ExpressionExts.cs
+++ Next.PCL/Extensions/ExpressionExts.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Linq.Expressions;
 using System.Reflection;
 using Common;
@@ -17,10 +19,76 @@ namespace Next.PCL.Extensions
             if (exp == null)
                 return string.Empty;
             if (exp.Body is MemberExpression m)
+            {
+                Console.WriteLine("{0}, {1}", m.Member.Name,exp.Body.NodeType);
                 return m.Member.Name;
+            }
             else if (exp.Body is UnaryExpression ue)
                 return ((MemberExpression)ue.Operand).Member.Name;
             return string.Empty;
+        }
+        public static TProp GetPropValue<TClass, TProp>(this TClass @class, Expression<Func<TClass>> propSelector)
+            where TClass : class
+        {
+            return @class.GetPropertyValue<TClass, TProp>(propSelector.GetPropName());
+        }
+        public static string GetPropName<TClass>(this Expression<Func<TClass>> expression)
+        {
+            var stack = new Stack<string>();
+            Expression expression1 = expression.Body;
+            while (expression1 != null)
+            {
+                if (expression1.NodeType == ExpressionType.Call)
+                {
+                    var methodCallExpression = (MethodCallExpression)expression1;
+                    if (IsSingleArgumentIndexer(methodCallExpression))
+                    {
+                        stack.Push(string.Empty);
+                        expression1 = methodCallExpression.Object;
+                    }
+                    else
+                        break;
+                }
+                else if (expression1.NodeType == ExpressionType.ArrayIndex)
+                {
+                    var binaryExpression = (BinaryExpression)expression1;
+                    stack.Push(string.Empty);
+                    expression1 = binaryExpression.Left;
+                }
+                else if (expression1.NodeType == ExpressionType.MemberAccess)
+                {
+                    var memberExpression = (MemberExpression)expression1;
+                    stack.Push("." + memberExpression.Member.Name);
+                    expression1 = memberExpression.Expression;
+                }
+                else if (expression1.NodeType == ExpressionType.Parameter)
+                {
+                    stack.Push(string.Empty);
+                    expression1 = null;
+                }
+                else if (expression1.NodeType == ExpressionType.Convert)
+                {
+                    var memberExp = ((UnaryExpression)expression1).Operand as MemberExpression;
+                    stack.Push("." + memberExp.Member.Name);
+                    expression1 = memberExp.Expression;
+                }
+                else
+                    break;
+            }
+            if (stack.Count > 0 && string.Equals(stack.Peek(), ".model", StringComparison.OrdinalIgnoreCase))
+                stack.Pop();
+            if (stack.Count <= 0)
+                return string.Empty;
+            string s = (stack).Aggregate(((left, right) => left + right)).TrimStart(new[] { '.' });
+            Console.WriteLine(s);
+            return s;
+        }
+        private static bool IsSingleArgumentIndexer(Expression expression)
+        {
+            var methodExpression = expression as MethodCallExpression;
+            if (methodExpression == null || methodExpression.Arguments.Count != 1)
+                return false;
+            return (methodExpression.Method.DeclaringType.GetDefaultMembers()).OfType<PropertyInfo>().Any((p => p.GetGetMethod() == methodExpression.Method));
         }
         public static Type GetPropType<TClass>(this Expression<Func<TClass, object>> exp)
             where TClass : class
