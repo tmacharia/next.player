da95e0d969d33727eb74f8e01b7f0b3edde3823f
tmacharia <timothy.macharia@outlook.com>
2021-06-05 06:24:21 +0300

    bug fix resolving app folder directory

--- Next.PCL/Configuration/FileSys.cs
+++ Next.PCL/Configuration/FileSys.cs
@@ -5,7 +5,17 @@ namespace Next.PCL
 {
     public class FileSys
     {
-        internal static string AppFolder => GetFolderPath(SpecialFolder.LocalApplicationData);
-        internal static string SettingsFolder => Path.Combine(AppFolder, "Settings");
+        private static readonly string _appFolder = Path.Combine(GetFolderPath(SpecialFolder.LocalApplicationData), "next");
+
+        internal static string AppFolder => ResolveFolder(_appFolder);
+
+        internal static string SettingsFolder => ResolveFolder(Path.Combine(_appFolder, "Settings"));
+
+        internal static string ResolveFolder(string path)
+        {
+            if (!Directory.Exists(path))
+                Directory.CreateDirectory(path);
+            return path;
+        }
     }
 }
