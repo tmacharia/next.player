be2c6da95b6a97e69fc8f42b42875b95d6e46d90
tmacharia <timothy.macharia@outlook.com>
2021-06-22 04:13:53 +0300

    re-use HttpClientHandler

--- Next.PCL/Online/Base/BaseOnline.cs
+++ Next.PCL/Online/Base/BaseOnline.cs
@@ -1,48 +1,64 @@
 ﻿using System;
+using System.Net;
 using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
+using Common;
 using Next.PCL.Exceptions;
 using Serilog;
 
 namespace Next.PCL.Online
 {
-    public class BaseOnline : IOnline
+    public abstract class BaseOnline : IOnline
     {
+        internal static HttpClientHandler DefaulthttpClientHandler= new HttpClientHandler()
+        {
+            AllowAutoRedirect = true,
+            MaxAutomaticRedirections = 2,
+            AutomaticDecompression = DecompressionMethods.GZip
+        };
+        private readonly HttpClientHandler _httpClientHandler;
+
+        public BaseOnline(HttpClientHandler httpClientHandler = default)
+        {
+            _httpClientHandler = httpClientHandler ?? DefaulthttpClientHandler;
+        }
         /// <summary></summary>
         /// <exception cref="OnlineException"/>
         internal async Task<string> GetAsync(Uri uri, CancellationToken token = default)
         {
-            using (HttpClient client = new HttpClient())
+            HttpClient client = GetHttpClient();
+
+            var req = new HttpRequestMessage(HttpMethod.Get, uri);
+            var res = await client.SendAsync(req, HttpCompletionOption.ResponseContentRead, token);
+            if (res.IsSuccessStatusCode)
+                return await res.Content.ReadAsStringAsync();
+            else
             {
-                var req = new HttpRequestMessage(HttpMethod.Get, uri);
-                var res = await client.SendAsync(req, HttpCompletionOption.ResponseContentRead, token);
-                if (res.IsSuccessStatusCode)
-                    return await res.Content.ReadAsStringAsync();
+                int code = (int)res.StatusCode;
+                if (code == 301 || code == 302)
+                {
+                    Log.Information("{0} Redirect to {1}", code, res.Headers.Location);
+                    return await GetAsync(res.Headers.Location, token);
+                }
                 else
                 {
-                    int code = (int)res.StatusCode;
-                    if(code == 301 || code == 302)
-                    {
-                        Log.Information("{0} Redirect to {1}", code, res.Headers.Location);
-                        return await GetAsync(res.Headers.Location, token);
-                    }
-                    else
-                    {
-                        Log.Error("http error {0} | {1}", res.StatusCode, res.ReasonPhrase);
-                        throw new OnlineException(code, res.ReasonPhrase);
-                    }
+                    Log.Error("http error {0} | {1}", res.StatusCode, res.ReasonPhrase);
+                    throw new OnlineException(code, res.ReasonPhrase);
                 }
             }
         }
         internal async Task<bool> PingAsync(Uri uri, CancellationToken token = default)
         {
-            using (HttpClient client=new HttpClient())
-            {
-                var req = new HttpRequestMessage(HttpMethod.Head, uri);
-                var res = await client.SendAsync(req, HttpCompletionOption.ResponseHeadersRead, token);
-                return res.IsSuccessStatusCode;
-            }
+            HttpClient client = GetHttpClient();
+            var req = new HttpRequestMessage(HttpMethod.Head, uri);
+            var res = await client.SendAsync(req, HttpCompletionOption.ResponseHeadersRead, token);
+            return res.IsSuccessStatusCode;
+        }
+
+        private HttpClient GetHttpClient()
+        {
+            return new HttpClient(_httpClientHandler, false);
         }
     }
 }
