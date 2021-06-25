c8fc72503f2355ac4aaf3a4ffd116f3a7dfe723a
tim <timothy.macharia@outlook.com>
2021-06-25 20:44:48 +0300

    search query cleaner & formatter

---
+++ Next.PCL/Services/QueryFormatResult.cs
@@ -0,0 +1,15 @@
+﻿namespace Next.PCL.Services
+{
+    public struct QueryFormatResult
+    {
+        public string Term { get; set; }
+        public int? Year { get; set; }
+
+        public override string ToString()
+        {
+            if (!Year.HasValue)
+                return Term;
+            return string.Format("{0} | {1}", Term, Year);
+        }
+    }
+}
