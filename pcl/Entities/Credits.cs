3a127a003a3bcb39888c9fa75cc77cca7b5de5fc
tmacharia <timothy.macharia@outlook.com>
2021-06-26 11:27:26 +0300

    credits entity

---
+++ pcl/Entities/Credits.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace Next.PCL.Entities
+{
+    public class Credits
+    {
+        public Credits() : base()
+        {
+            Cast = new List<Cast>();
+            Crew = new List<FilmMaker>();
+        }
+        public List<Cast> Cast { get; set; }
+        public List<FilmMaker> Crew { get; set; }
+
+        public override string ToString()
+        {
+            return string.Format("{0:N0} cast, {1:N0} crew", Cast.Count, Crew.Count);
+        }
+    }
+}
