c974588d0ed2dc89aafd8f4e6af559a03040efa3
tmacharia <timothy.macharia@outlook.com>
2021-06-26 08:45:56 +0300

    tmdb config

---
+++ pcl/Configuration/TmdbConfig.cs
@@ -0,0 +1,13 @@
+﻿namespace Next.PCL.Configurations
+{
+    public class TmdbConfig
+    {
+        public TmdbConfig()
+        {
+            Language = "en";
+            TvShowSpecials = true;
+        }
+        public string Language { get; set; }
+        public bool TvShowSpecials { get; set; }
+    }
+}
