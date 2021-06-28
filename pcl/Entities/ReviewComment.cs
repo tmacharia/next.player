98fdc7be687f616acd8dbee5bf2eaef265e6ac14
tim <timothy.macharia@outlook.com>
2021-06-28 19:28:40 +0300

    review comment

---
+++ pcl/Entities/ReviewComment.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Next.PCL.Entities
+{
+    public class ReviewComment : RootEntity<string>
+    {
+        public Uri Url { get; set; }
+        public double? Score { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public string Author { get; set; }
+        public new DateTime? Timestamp { get; set; }
+
+        public override string ToString()
+        {
+            return string.Format("{0:N0}/10 by {1}, {2}", Score.HasValue ? Score.Value.ToString() : "?", Author, Content.Substring(0, 10));
+        }
+    }
+}
