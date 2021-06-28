6b562c16b8d9d0cb548af54564a8c1d430110dce
tim <timothy.macharia@outlook.com>
2021-06-28 19:25:58 +0300

    standard cast & film maker entities

--- pcl/Entities/Cast.cs
+++ pcl/Entities/Cast.cs
@@ -1,28 +1,15 @@
-﻿using Newtonsoft.Json;
-using System;
-
-namespace Next.PCL.Entities
+﻿namespace Next.PCL.Entities
 {
     public class Cast : Person
     {
-        public int Order { get; set; }
-        [JsonProperty(
-            NullValueHandling = NullValueHandling.Ignore,
-            DefaultValueHandling = DefaultValueHandling.IgnoreAndPopulate)]
-        public string Character { get; set; }
-    }
-    public class ReviewComment : RootEntity<string>
-    {
-        public Uri Url { get; set; }
-        public double? Score { get; set; }
-        public string Title { get; set; }
-        public string Content { get; set; }
-        public string Author { get; set; }
-        public new DateTime? Timestamp { get; set; }
-
-        public override string ToString()
+        public Cast() :base()
+        { }
+        public Cast(Person person, string character)
+            :base(person)
         {
-            return string.Format("{0:N0}/10 by {1}, {2}", Score.HasValue ? Score.Value.ToString() : "?", Author, Content.Substring(0, 10));
+            Role = character;
         }
+
+        public int Order { get; set; }
     }
 }
--- pcl/Entities/Person.cs
+++ pcl/Entities/Person.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using Next.PCL.Extensions;
 using Next.PCL.Metas;
 
 namespace Next.PCL.Entities
@@ -11,9 +12,27 @@ namespace Next.PCL.Entities
             Urls = new List<MetaUrl>();
             Images = new List<MetaImage>();
         }
+        public Person(Person person) :this()
+        {
+            if(person != null)
+            {
+                Id = person.Id;
+                Name = person.Name;
+                if (person.Urls.IsNotNullOrEmpty())
+                    Urls.AddRange(person.Urls);
+                if (person.Images.IsNotNullOrEmpty())
+                    Images.AddRange(person.Images);
+            }
+        }
+        public virtual string Role { get; set; }
         public DateTime? LastModified { get; set; }
 
         public List<MetaUrl> Urls { get; set; }
         public List<MetaImage> Images { get; set; }
+
+        public override string ToString()
+        {
+            return string.Format("{0}, {1} as {2}", Id, Name, Role);
+        }
     }
 }
