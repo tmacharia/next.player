111f0c1b3a923d146f050618d4e96e1415a4653c
tmacharia <timothy.macharia@outlook.com>
2021-06-30 06:56:38 +0300

    winmd storage item exts

---
+++ wrc/StorageFileExts.cs
@@ -0,0 +1,20 @@
+﻿using Windows.Foundation;
+using Windows.Storage;
+using Windows.Storage.Pickers;
+
+namespace Next.WRC
+{
+    public sealed class StorageFileExts
+    {
+        public static IAsyncOperation<StorageFile> PickFileAsync()
+        {
+            FileOpenPicker picker = new FileOpenPicker
+            {
+                ViewMode = PickerViewMode.Thumbnail,
+                SuggestedStartLocation = PickerLocationId.ComputerFolder
+            };
+            picker.FileTypeFilter.Add(".mkv");
+            return picker.PickSingleFileAsync();
+        }
+    }
+}
---
+++ wrc/StorageFolderExts.cs
@@ -0,0 +1,19 @@
+﻿using Windows.Foundation;
+using Windows.Storage;
+using Windows.Storage.Pickers;
+
+namespace Next.WRC
+{
+    public sealed class StorageFolderExts
+    {
+        public static IAsyncOperation<StorageFolder> PickFolderAsync()
+        {
+            FolderPicker picker = new FolderPicker
+            {
+                ViewMode = PickerViewMode.Thumbnail,
+                SuggestedStartLocation = PickerLocationId.ComputerFolder
+            };
+            return picker.PickSingleFolderAsync();
+        }
+    }
+}
