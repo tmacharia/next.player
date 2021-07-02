3376c2c85a6d96a43614ede4dc9037d171247a3c
tmacharia <timothy.macharia@outlook.com>
2021-07-02 07:06:02 +0300

    refactor imdb for code reuse

--- pcl/Online/Imdb.cs
+++ pcl/Online/Imdb.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using Common;
 using Next.PCL.Entities;
 using Next.PCL.Html;
 using Next.PCL.Online.Models.Imdb;
@@ -20,31 +22,40 @@ namespace Next.PCL.Online
 
         public async Task<ImdbModel> GetImdbAsync(string imdbId, CancellationToken cancellationToken = default)
         {
-            var uri = new Uri(string.Format("{0}/title/{1}", SiteUrls.IMDB, imdbId));
-            string html = await GetAsync(uri, cancellationToken);
+            string html = await GetAsync(GenerateUrl(imdbId), cancellationToken);
             return _parser.ParseImdb(html);
         }
         public async Task<List<ImdbSuggestion>> GetSuggestionsAsync(string imdbId, CancellationToken cancellationToken = default)
         {
-            var uri = new Uri(string.Format("{0}/title/{1}", SiteUrls.IMDB, imdbId));
-            string html = await GetAsync(uri, cancellationToken);
-            return _parser.ParseSuggestions(html).ToList();
+            var list = await _parser.ParseSuggestionsAsync(GenerateUrl(imdbId), cancellationToken);
+            return list.ToList();
         }
 
         public async Task<List<ImdbReview>> GetReviewsAsync(string imdbId, CancellationToken cancellationToken = default)
         {
-            var uri = new Uri(string.Format("{0}/title/{1}/reviews", SiteUrls.IMDB, imdbId));
-            string html = await GetAsync(uri, cancellationToken);
+            string html = await GetAsync(GenerateUrl(imdbId, "reviews"), cancellationToken);
             return _parser.ParseReviews(html).ToList();
         }
         public async Task<List<GeographicLocation>> GetLocationsAsync(string imdbId, CancellationToken cancellationToken = default)
         {
-            var uri = new Uri(string.Format("{0}/title/{1}/locations", SiteUrls.IMDB, imdbId));
-            string html = await GetAsync(uri, cancellationToken);
+            string html = await GetAsync(GenerateUrl(imdbId, "locations"), cancellationToken);
             return _parser.ParseFilmingLocations(html)
                           .OrderBy(x => x.Name)
                           .ThenBy(x => x.Inner.Count)
                           .ToList();
+        }
+
+        internal Uri GenerateUrl(string imdbId, string suffix = default)
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendFormat("{0}/title/{1}", SiteUrls.IMDB, imdbId);
+            if (suffix.IsValid())
+            {
+                if (!suffix.StartsWith("/"))
+                    sb.Append("/");
+                sb.Append(suffix);
+            }
+            return new Uri(sb.ToString());
         }
     }
 }
