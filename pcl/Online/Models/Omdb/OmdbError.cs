9faa20978e792ad9a9ee558f8db3d141faf2cd1d
tim <timothy.macharia@outlook.com>
2021-07-07 22:52:31 +0300

    omdb error model

---
+++ pcl/Online/Models/Omdb/OmdbError.cs
@@ -0,0 +1,12 @@
+﻿using Newtonsoft.Json;
+
+namespace Next.PCL.Online.Models
+{
+    public class OmdbError
+    {
+        [JsonProperty("Response")]
+        public bool IsSuccess { get; set; }
+        [JsonProperty("Error")]
+        public string ErrorMessage { get; set; }
+    }
+}
