39f3b4f31a9ed9d83a308e534a42208fe999be33
tmacharia <timothy.macharia@outlook.com>
2021-07-15 02:03:59 +0300

    gender & birthday for person entity

--- pcl/Entities/Person.cs
+++ pcl/Entities/Person.cs
@@ -1,12 +1,23 @@
-﻿namespace Next.PCL.Entities
+﻿using System;
+using Next.PCL.Enums;
+
+namespace Next.PCL.Entities
 {
     public class Person : RootPersonEntity
     {
         public Person() :base()
         { }
         public Person(Person person) :base(person)
-        { }
+        { 
+            if(person != null)
+            {
+                Gender = person.Gender;
+                Birthday = person.Birthday;
+            }
+        }
         public virtual string Role { get; set; }
+        public Gender Gender { get; set; }
+        public DateTime? Birthday { get; set; }
 
         public override string ToString()
         {
